d4d26a4c6f38896bf5547a3cdb860b5403f53fe6
sergi <sergi.argerich@gmail.com>
2020-05-04 21:24:44 +0200

    Modify the locale listener adding the locale from the query param _locale to the current request.

--- app/src/EventSubscriber/LocaleSubscriber.php
+++ app/src/EventSubscriber/LocaleSubscriber.php
@@ -22,8 +22,9 @@ class LocaleSubscriber implements EventSubscriberInterface
         }
 
         // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
+        if ($locale = $request->query->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
+            $request->setLocale($locale);
         } else {
             // if no explicit locale has been set on this request, use one from the session
             $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
